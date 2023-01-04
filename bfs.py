a56bf36b8ca2e453a35c7adf8de55ff0bb24533f
Thenu <anbunachiappan@gmail.com>
2023-01-04 14:29:16 -0800

    dfs1

--- bfs.py
+++ bfs.py
@@ -38,3 +38,46 @@ def bfs(graph, node): #function for BFS
 # Driver Code
 print("Following is the Breadth-First Search")
 bfs( graph, '5')    # function calling
+
+
+
+
+
+# const adjList = {
+#   1: [2, 5],
+#   2: [1, 3, 5],
+#   3: [2, 4],
+#   4: [3, 5, 6],
+#   5: [1, 2, 4],
+#   6: [4]
+# }
+
+
+# function printBreadthFirst(start) {
+# const queue = [];
+#   queue.push(start);
+#   let visisted = new Set();
+#   visisted.add(start);
+#   while(queue.length){
+#   let node = queue.shift();
+#     console.log(node);
+#     const neighbours = adjList[node]
+#   neighbours.forEach(e=>{
+#   if(!visisted.has(e)){
+#   queue.push(e);
+#     visisted.add(e)
+#   }
+  
+#   })
+#   }
+    
+# };
+# console.log("First Test:")
+# printBreadthFirst(3); // Prints 1 through 6 in Breadth-first order, starting with 3
+#                       // One possible solution:  3, 2, 4, 1, 5, 6
+# console.log("Second Test:")
+# printBreadthFirst(6); // Prints 1 through 6 in Breadth-first order, starting with 6
+#                       // One possible solution:  6, 4, 3, 5, 2, 1
+# console.log("Third Test:")
+# printBreadthFirst(4); // Prints 1 through 6 in Breadth-first order, starting with 4
+#                       // One possible solution:  4, 3, 5, 6, 2, 1
---
+++ leetcode/sameTree.js
@@ -0,0 +1,14 @@
+// bfs
+// iterative
+//queue
+//level by level
+
+
+var isSameTree = function(p, q) {
+    
+  
+};
+
+p =[1,2];
+q =[1,null,2]
+isSameTree(p,q)
