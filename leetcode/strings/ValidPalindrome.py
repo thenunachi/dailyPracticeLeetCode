63ec6f001f4cd7346c6b4d1a7687b33d5d612b0f
Thenu <anbunachiappan@gmail.com>
2023-04-06 11:46:15 -0700

    fdgdgfd

--- leetcode/strings/ValidPalindrome.py
+++ leetcode/strings/ValidPalindrome.py
@@ -25,3 +25,18 @@ class Solution:
     #   print(ord("A"),"a")
     #   print(ord(c),"c")
       return ((ord("A") <= ord(c) <=ord("Z")) or    (ord("a") <= ord(c) <=ord("z")) or     (ord("0") <= ord(c) <=ord("9"))    )
+    
+
+
+     left, right = 0, len(s) - 1 
+ 
+    while left < right:
+        while left < right and not s[left].isalnum():
+            left +=1
+        while right > left and not s[right].isalnum():
+            right -= 1
+        if s[left].lower() != s[right].lower():
+            return False 
+        left, right = left + 1, right - 1
+    return True
+The time complexity for this solution is O(n) because you are still looping through the entire string. The space complexity is O(1) because you are not making a new string.
--- leetcode/strings/longestCommonPrefix.py
+++ leetcode/strings/longestCommonPrefix.py
@@ -15,14 +15,25 @@
 
 
 def longestCommonPrefix(strs):
-  res=""
-  for i in range(len(strs[0])):
-    for s in strs:
-        if i == len(s) or s[i] != strs[0][i]: #without this check for i==len(s) also works
-            return res
+  # res=""
+  # for i in range(len(strs[0])):
+  #   for s in strs:
+  #       if i == len(s) or s[i] != strs[0][i]: #without this check for i==len(s) also works
+  #           return res
        
-    res += strs[0][i]
-  return res
+  #   res += strs[0][i]
+  # return res
+        prefix = strs[0]
+        for i in range(1,len(strs)):
+          current = strs[i]
+          res = ""
+          for j in range(len(current)):
+            if j < len(prefix) and current[j] == prefix[j]:
+              res +=prefix[j]
+            else:
+              break
+          prefix = res
+        return prefix
 strs = ["flower","flow","flight"]
 print(longestCommonPrefix(strs))
 
