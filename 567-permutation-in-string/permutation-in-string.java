061714b70f780fcdb5626a23acb059aaf3d9f791
Thenu <64111530+thenunachi@users.noreply.github.com>
2023-07-21 14:22:38 -0700

    Time: 5 ms (96.78%) | Memory: 41.6 MB (83.98%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int len1 = s1.length();
+        int len2 = s2.length();
+        
+        if (len1 > len2)
+            return false;
+        
+        int[] count = new int[26];
+        
+        // Count frequencies of characters in s1
+        for (char c : s1.toCharArray())
+            count[c - 'a']++;
+        
+        // Use sliding window technique
+        for (int i = 0; i < len2; i++) {
+            // Add current character to the window
+            count[s2.charAt(i) - 'a']--;
+            
+            // Remove leftmost character if window size exceeds s1 length
+            if (i >= len1)
+                count[s2.charAt(i - len1) - 'a']++;
+            
+            // Check if current window is an anagram of s1
+            if (i >= len1 - 1 && allZero(count))
+                return true;
+        }
+        
+        return false;
+    }
+    
+    private boolean allZero(int[] count) {
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] != 0)
+                return false;
+        }
+        return true;
+    }
+}
