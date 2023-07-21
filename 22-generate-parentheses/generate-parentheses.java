93d83380241034999d8134a4a74e9f3b2b21e7aa
Thenu <64111530+thenunachi@users.noreply.github.com>
2023-07-21 13:14:41 -0700

    Time: 1 ms (95.84%) | Memory: 43.6 MB (44.74%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,54 @@
+class Solution {
+   public List<String> generateParenthesis(int n) {
+        // Resultant list
+        List<String> result = new ArrayList<>();
+        /// Recursively generate parentheses
+        generateParenthesis(result, "", 0, 0, n);
+        return result;
+    }
+
+    private void generateParenthesis(List<String> result, String s, int open, int close, int n) {
+        // Base case
+        if (open == n && close == n) {
+            result.add(s);
+//             System.out.println(result);
+//             [((()))]
+// [((())), (()())]
+// [((())), (()()), (())()]
+// [((())), (()()), (())(), ()(())]
+// [((())), (()()), (())(), ()(()), ()()()]
+            return;
+        }
+        // If the number of open parentheses is less than the given n
+        if (open < n) {
+            generateParenthesis(result, s + "(", open + 1, close, n);
+            // System.out.println(result);
+//             [((()))]
+// [((())), (()())]
+// [((())), (()()), (())()]
+// [((())), (()()), (())()]
+// [((())), (()()), (())(), ()(())]
+// [((())), (()()), (())(), ()(()), ()()()]
+// [((())), (()()), (())(), ()(()), ()()()]
+// [((())), (()()), (())(), ()(()), ()()()]
+        }
+        // If we need more close parentheses to balance
+        if (close < open) {
+            generateParenthesis(result, s + ")", open, close + 1, n);
+//                   System.out.println(result);
+//                   [((()))]
+// [((()))]
+// [((()))]
+// [((())), (()())]
+// [((())), (()())]
+// [((())), (()()), (())()]
+// [((())), (()()), (())()]
+// [((())), (()()), (())()]
+// [((())), (()()), (())(), ()(())]
+// [((())), (()()), (())(), ()(())]
+// [((())), (()()), (())(), ()(()), ()()()]
+// [((())), (()()), (())(), ()(()), ()()()]
+// [((())), (()()), (())(), ()(()), ()()()]
+        }
+    }
+}
