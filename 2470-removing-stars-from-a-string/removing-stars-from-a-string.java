d2c713b41f5575b069d1f9b91e69065cb5953b4e
Thenu <64111530+thenunachi@users.noreply.github.com>
2023-07-25 13:40:53 -0700

    Time: 200 ms (42.11%) | Memory: 45.6 MB (46.46%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
@@ -2,6 +2,7 @@ class Solution {
     public String removeStars(String s) {
         StringBuilder result = new StringBuilder();
         Stack<Character> stack = new Stack<>();
+        // System.out.println(s.toCharArray()); leet**cod*e
         for(char ch : s.toCharArray()){
             if(ch == '*') {
                 stack.pop();
@@ -9,8 +10,10 @@ class Solution {
                 stack.push(ch);
             }
         }
+        // System.out.println(stack);[l, e, c, o, e]
         while(!stack.isEmpty()){
             result.append(stack.pop());
+            // System.out.println(result); eocel
         }
         return result.reverse().toString();
     }
