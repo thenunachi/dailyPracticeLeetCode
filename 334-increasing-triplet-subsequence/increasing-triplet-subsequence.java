d99ef3674a6c68363aff04955fe109a4d8868925
Thenu <64111530+thenunachi@users.noreply.github.com>
2023-07-26 14:11:04 -0700

    Time: 3 ms (49.37%) | Memory: 131.7 MB (49.64%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -1,25 +1,31 @@
 class Solution {
-   public boolean increasingTriplet(int[] nums) {
-    if (nums == null || nums.length < 3) {
+    public boolean increasingTriplet(int[] nums) {
+        if(nums == null || nums.length < 3)return false;
+        int min1 = Integer.MAX_VALUE;
+       int min2 = Integer.MAX_VALUE;
+        for(int n : nums){
+            if( n < min1){
+                min1 = n;
+            }
+             if(n > min1){
+                min2 = Math.min(min2,n);
+            }
+             if(n > min2){
+                return true;
+            }
+        }
         return false;
     }
-        
-    int minOne = Integer.MAX_VALUE;
-    int minTwo = Integer.MAX_VALUE;
-        
-    for (int num : nums) {
-        if (num < minOne) {
-            minOne = num;
-        }
+}
+
+//  if (num < minOne) {
+//             minOne = num;
+//         }
             
-        if (num > minOne) {
-            minTwo = Math.min(num, minTwo);
-        }
+//         if (num > minOne) {
+//             minTwo = Math.min(num, minTwo);
+//         }
           
-        if (num > minTwo) {
-            return true;
-        }
-    }
-    return false;
-}
-}
+//         if (num > minTwo) {
+//             return true;
+//         }
