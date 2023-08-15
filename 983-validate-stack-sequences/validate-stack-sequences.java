07d62666e115ece1f82ca1ccb94c9c6efa76a7a7
Thenu <64111530+thenunachi@users.noreply.github.com>
2023-08-15 14:29:30 -0700

    Time: 2 ms (86.94%) | Memory: 43 MB (76.50%) - LeetSync

--- 983-validate-stack-sequences/validate-stack-sequences.java
+++ 983-validate-stack-sequences/validate-stack-sequences.java
@@ -1,16 +1,20 @@
 class Solution {
   public boolean validateStackSequences(int[] pushed, int[] popped) {
-    Deque<Integer> stack = new ArrayDeque<>();
-    int i = 0; // popped's index
+    Deque<Integer> stack = new ArrayDeque<>(); // Create an empty stack using a Deque
+    int i = 0; // Index to traverse the popped array
 
+    // Loop through each element in the pushed array
     for (final int x : pushed) {
-      stack.push(x);
+      stack.push(x); // Push the current element onto the stack
+
+      // Check if the stack is not empty and the top element matches the current element in the popped array
       while (!stack.isEmpty() && stack.peek() == popped[i]) {
-        stack.pop();
-        ++i;
+        stack.pop(); // Pop the top element from the stack
+        ++i; // Move to the next index in the popped array
       }
     }
 
+    // After processing all elements in the pushed array, if the stack is empty, the sequence is valid
     return stack.isEmpty();
   }
 }
