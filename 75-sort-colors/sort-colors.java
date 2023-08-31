3911d4f83fc3f5726778f67408e3d77b4e86a07f
Thenu <64111530+thenunachi@users.noreply.github.com>
2023-08-31 14:54:22 -0700

    Time: 0 ms (100.00%) | Memory: 40.4 MB (98.92%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,59 @@
+// class Solution {
+//     public void sortColors(int[] nums) {
+//       int count0 = 0;
+//       int count1 = 0;
+//       int count2 = 0;
+//       for(int i =0 ; i< nums.length;i++){
+//           if(nums[i] == 0){
+//               count0++;
+//           }
+//       } 
+//       for(int i =0 ; i< nums.length;i++){
+//           if(nums[i] == 1){
+//               count1++;
+//           }
+//       } 
+//       for(int i =0 ; i< nums.length;i++){
+//           if(nums[i] == 2){
+//               count2++;
+//           }
+//       }  
+
+//       for(int i =0 ; i< count0;i++){
+//           nums[i] = 0;
+//       }
+//       for(int i =count1 ; i< count2;i++){
+//           nums[i] = 1;
+//       }
+//       for(int i =count2 ; i< nums.length;i++){
+//           nums[i] = 2;
+//       }
+//     }
+// }
+class Solution {
+    public void sortColors(int[] nums) {
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        
+        for (int num : nums) {
+            if (num == 0) {
+                count0++;
+            } else if (num == 1) {
+                count1++;
+            } else if (num == 2) {
+                count2++;
+            }
+        }
+        
+        for (int i = 0; i < count0; i++) {
+            nums[i] = 0;
+        }
+        for (int i = count0; i < count0 + count1; i++) {
+            nums[i] = 1;
+        }
+        for (int i = count0 + count1; i < nums.length; i++) {
+            nums[i] = 2;
+        }
+    }
+}
