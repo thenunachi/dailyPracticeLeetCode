83eab3d8eef37d569528582b299f0f8ac15fa74e
Thenu <64111530+thenunachi@users.noreply.github.com>
2023-10-25 14:27:30 -0700

    Time: 8 ms (91.74%) | Memory: 45.4 MB (62.88%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,33 +1,20 @@
-import java.util.*;
-
-public class Solution {
+class Solution {
     public int[][] merge(int[][] intervals) {
-        if (intervals.length <= 1) {
-            return intervals;
-        }
-
-        // Sort the array so that earlier start times are at the beginning
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-
-        List<int[]> output = new ArrayList<>();
-        int[] current = intervals[0];
-        output.add(current);
-
-        // If the current interval's end time is greater than or equal to
-        // the next interval's start time, then we know there is an overlap
-        // and we merge them. If there is no overlap, then we add the next
-        // interval to the list of intervals in our output array.
-        for (int i = 1; i < intervals.length; i++) {
-            int[] next = intervals[i];
-            if (current[1] >= next[0]) {
-                 current[0] = Math.min(current[0], next[0]);
-            current[1] = Math.max(current[1], next[1]);
-            } else {
-                current = next;
-                output.add(current);
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        List<int[]>output = new ArrayList<>();
+        int [] curr = intervals[0];
+        output.add(curr);
+        for(int i =1; i <intervals.length;i++){
+            if(curr[1]>=intervals[i][0]){
+                curr[0] = Math.min(curr[0],intervals[i][0]);
+                curr[1] = Math.max(curr[1],intervals[i][1]);
             }
-        }
+            else{
+                curr = intervals[i];
+                output.add(curr);
+            }
 
-        return output.toArray(new int[0][]);
+        }
+        return output.toArray(new int [0][]);
     }
 }
