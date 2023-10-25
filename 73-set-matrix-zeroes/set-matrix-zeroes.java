f5810f03f62aef1b87edd46bfc87cfe364180d8b
Thenu <64111530+thenunachi@users.noreply.github.com>
2023-10-24 22:14:22 -0700

    Time: 2 ms (22.61%) | Memory: 44.4 MB (64.72%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,29 +1,22 @@
-public class Solution {
-    public static int[][] setZeroes(int[][] matrix) {
-        HashSet<Integer> rowMap = new HashSet<>();
-        HashSet<Integer> colMap = new HashSet<>();
-
-        int row = matrix.length;
-        int col = matrix[0].length;
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (matrix[i][j] == 0) {
-                    rowMap.add(i);
-                    colMap.add(j);
-                }
-            }
-        }
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (rowMap.contains(i) || colMap.contains(j)) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        return matrix;
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int R =  matrix.length;
+        int C = matrix[0].length;
+        HashSet<Integer> rSet = new HashSet<>();
+         HashSet<Integer> cSet = new HashSet<>();
+         for(int i=0;i<R;i++){
+             for(int j=0;j<C; j++){
+                 if(matrix[i][j] == 0){
+                     rSet.add(i);
+                     cSet.add(j);
+                 }
+             }
+         }
+          for(int i=0;i<R;i++){
+             for(int j=0;j<C; j++){
+                 if(rSet.contains(i) || cSet.contains(j)){
+                     matrix[i][j] =0;
+                 }
+             }}
     }
 }
-//he time complexity of the code is O(nm) and the space complexity is O(n + m).
