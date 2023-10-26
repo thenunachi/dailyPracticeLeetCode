b85b8a3c3a3a0ab8f4f12b7e613e1d37c993e65a
Thenu <64111530+thenunachi@users.noreply.github.com>
2023-10-26 14:41:31 -0700

    Time: 0 ms (100.00%) | Memory: 40.1 MB (24.13%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int n = nums.length;
+        int[] dp = new int[n + 1];
+// Set dp[0] to 0, since the maximum amount of money that can be robbed up to the 0th house is 0.
+// Set dp[1] to the value of nums[0], since the maximum amount of money that can be robbed up to the 1st house is simply the value of the 1st house.
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+        }
+
+        return dp[n];
+    }
+}
+
+// Input: nums = [1, 2, 3, 1]
+
+// dp = [0, 1, 3, 4]
+
+// dp[0] = 0
+// dp[1] = nums[0] = 1
+// dp[2] = max(dp[1], dp[0] + nums[1]) = max(1, 0 + 2) = 2
+// dp[3] = max(dp[2], dp[1] + nums[2]) = max(2, 1 + 3) = 4
+
+// Output: 4
