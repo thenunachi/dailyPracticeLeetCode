48685e8ac67db14143a24c6be67b01e04b246610
Thenu <64111530+thenunachi@users.noreply.github.com>
2023-10-30 18:38:58 -0700

    Time: 0 ms (100.00%) | Memory: 46.4 MB (15.23%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -14,41 +14,20 @@
  * }
  */
 class Solution {
-
     public int maxPathSum(TreeNode root) {
-        // Initialize the result to the lowest possible value.
-        int[] res = { Integer.MIN_VALUE };
-
-        // Recursively find the maximum path sum for the given root node.
-        dfs(root, res);
-
-        // Return the maximum path sum.
+        int []res = {Integer.MIN_VALUE};
+        dfs(root,res);
         return res[0];
     }
-
-    /**
-     * Recursively finds the maximum path sum for the given root node.
-     *
-     * @param root The root node of the subtree.
-     * @param res An array to store the maximum path sum so far.
-     * @return The maximum path sum for the given root node.
-     */
-    public int dfs(TreeNode root, int[] res) {
-        // If the root node is null, then the maximum path sum is 0.
-        if (root == null) {
-            return 0;
-        }
-
-        // Recursively find the maximum path sum for the left and right subtrees.
-        int left = Math.max(0, dfs(root.left, res));
-        //the 0 is used as a boundary or threshold value to ensure that negative values are not included in the calculation of the left variable. 
-        int right = Math.max(0, dfs(root.right, res));
-
-        // Update the result variable with the maximum path sum that goes through the current node.(with split into left and right)
-        res[0] = Math.max(res[0], root.val + left + right);
-
-        // Return the maximum path sum that starts from the current node and goes down to one of its children.(without split)
-        return root.val + Math.max(left, right);
+    private int dfs(TreeNode root,int [] res){
+        if(root == null)return 0;
+        int leftMax = dfs(root.left,res);
+        int rightMax = dfs(root.right,res);
+        int left = Math.max(0,leftMax);
+        int right = Math.max(0,rightMax);
+        //with split
+        res[0] = Math.max(res[0],root.val+left+right);
+        //without split
+        return root.val+ Math.max(left,right);
     }
 }
-//time =O(n) space =O(h)h=height of tree
