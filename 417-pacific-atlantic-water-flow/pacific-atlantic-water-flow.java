634835182d72939581bfab8c0c919657014062bc
Thenu <64111530+thenunachi@users.noreply.github.com>
2023-11-02 13:02:39 -0700

    Time: 4 ms (97.56%) | Memory: 45 MB (28.15%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -3,36 +3,34 @@ class Solution {
         List<List<Integer>> res = new ArrayList<>();
         int r = heights.length;
         int c = heights[0].length;
-        boolean[][] pacific = new boolean[r][c];
-        boolean[][] atlantic = new boolean[r][c];
-
-        for (int i = 0; i < c; i++) {
-            dfs(0, i, pacific, heights, Integer.MIN_VALUE, r, c);
-            dfs(r - 1, i, atlantic, heights, Integer.MIN_VALUE, r, c);
-        }
-        for (int i = 0; i < r; i++) {
-            dfs(i, 0, pacific, heights, Integer.MIN_VALUE, r, c);
-            dfs(i, c - 1, atlantic, heights, Integer.MIN_VALUE, r, c);
-        }
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (pacific[i][j] && atlantic[i][j]) {
-                    res.add(Arrays.asList(i, j));
-                }
-            }
-        }
-        return res;
+        boolean [][] pacific = new boolean [r][c];
+         boolean [][] atlantic = new boolean [r][c];
+         for(int i=0;i<c;i++){
+             dfs(0,i,r,c,heights,pacific,Integer.MIN_VALUE);
+           dfs(r-1,i,r,c,heights,atlantic,Integer.MIN_VALUE);
+         }
+            for(int i=0;i<r;i++){
+             dfs(i,0,r,c,heights,pacific,Integer.MIN_VALUE);
+           dfs(i,c-1,r,c,heights,atlantic,Integer.MIN_VALUE);
+         }
+         for(int i=0;i<r;i++){
+             for(int j =0;j<c;j++){
+                 if(pacific[i][j] && atlantic[i][j]){
+                     res.add(Arrays.asList(i,j));
+                 }
+             }
+         }
+         return res;
     }
-
-    private void dfs(int i, int j, boolean[][] ocean, int[][] heights, int prev, int r, int c) {
-        if (i < 0 || j < 0 || i >= r || j >= c || ocean[i][j] || heights[i][j] < prev) {
+    private void  dfs(int i,int j,int r,int c,int[][] heights, boolean [][] ocean,int prev){
+        if(i<0 || j< 0 || j>=c||i>=r || ocean[i][j] || heights[i][j] < prev){
             return;
         }
         ocean[i][j] = true;
-
-        dfs(i + 1, j, ocean, heights, heights[i][j], r, c);
-        dfs(i - 1, j, ocean, heights, heights[i][j], r, c);
-        dfs(i, j + 1, ocean, heights, heights[i][j], r, c);
-        dfs(i, j - 1, ocean, heights, heights[i][j], r, c);
+         dfs(i+1,j,r,c,heights,ocean,heights[i][j]);
+               dfs(i-1,j,r,c,heights,ocean,heights[i][j]);
+                     dfs(i,j+1,r,c,heights,ocean,heights[i][j]);
+                           dfs(i,j-1,r,c,heights,ocean,heights[i][j]);
+                        
     }
 }
