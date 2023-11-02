93c6b2ae9c7689a4f49a868c8556b242c68f2c2f
Thenu <64111530+thenunachi@users.noreply.github.com>
2023-11-02 11:13:06 -0700

    Time: 4 ms (97.55%) | Memory: 45.3 MB (8.59%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,54 +1,38 @@
 class Solution {
-  int[][] dir = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        List<List<Integer>> res = new ArrayList<>();
+        int r = heights.length;
+        int c = heights[0].length;
+        boolean[][] pacific = new boolean[r][c];
+        boolean[][] atlantic = new boolean[r][c];
 
-  public List<List<Integer>> pacificAtlantic(int[][] heights) {
-    List<List<Integer>> res = new ArrayList<>();
-    int rows = heights.length;
-    int cols = heights[0].length;
-    boolean[][] pacific = new boolean[rows][cols];
-    boolean[][] atlantic = new boolean[rows][cols];
-
-    // Loop to start DFS from the Pacific coast
-    for (int i = 0; i < cols; i++) {
-      dfs(heights, 0, i, Integer.MIN_VALUE, pacific);
-    }
-
-    // Loop to start DFS from the Atlantic coast
-    for (int i = 0; i < cols; i++) {
-      dfs(heights, rows - 1, i, Integer.MIN_VALUE, atlantic);
-    }
-
-    // Loop to start DFS from the left and right boundaries
-    for (int i = 0; i < rows; i++) {
-      dfs(heights, i, 0, Integer.MIN_VALUE, pacific);
-      dfs(heights, i, cols - 1, Integer.MIN_VALUE, atlantic);
-    }
-
-    // Check cells that can reach both the Pacific and Atlantic
-    for (int i = 0; i < rows; i++) {
-      for (int j = 0; j < cols; j++) {
-        if (pacific[i][j] && atlantic[i][j]) {
-          res.add(Arrays.asList(i, j));
+        for (int i = 0; i < c; i++) {
+            dfs(0, i, pacific, heights, Integer.MIN_VALUE, r, c);
+            dfs(r - 1, i, atlantic, heights, Integer.MIN_VALUE, r, c);
         }
-      }
-    }
-    return res;
-  }
-
-  private boolean dfs(int[][] heights, int i, int j, int prev, boolean[][] ocean) {
-    if (i < 0 || i >= ocean.length || j < 0 || j >= ocean[0].length)
-      return false;
-
-    if (heights[i][j] < prev || ocean[i][j])
-      return false;
-
-    ocean[i][j] = true;
-
-    // Explore in all four directions
-    for (int[] d : dir) {
-      dfs(heights, i + d[0], j + d[1], heights[i][j], ocean);
+        for (int i = 0; i < r; i++) {
+            dfs(i, 0, pacific, heights, Integer.MIN_VALUE, r, c);
+            dfs(i, c - 1, atlantic, heights, Integer.MIN_VALUE, r, c);
+        }
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    res.add(Arrays.asList(i, j));
+                }
+            }
+        }
+        return res;
     }
 
-    return true;
-  }
+    private void dfs(int i, int j, boolean[][] ocean, int[][] heights, int prev, int r, int c) {
+        if (i < 0 || j < 0 || i >= r || j >= c || ocean[i][j] || heights[i][j] < prev) {
+            return;
+        }
+        ocean[i][j] = true;
+
+        dfs(i + 1, j, ocean, heights, heights[i][j], r, c);
+        dfs(i - 1, j, ocean, heights, heights[i][j], r, c);
+        dfs(i, j + 1, ocean, heights, heights[i][j], r, c);
+        dfs(i, j - 1, ocean, heights, heights[i][j], r, c);
+    }
 }
