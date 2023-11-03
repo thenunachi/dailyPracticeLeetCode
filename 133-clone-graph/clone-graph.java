f848921355a8cb21239fba9dfb1ada6584024e1b
Thenu <64111530+thenunachi@users.noreply.github.com>
2023-11-03 10:58:37 -0700

    Time: 26 ms (66.08%) | Memory: 41.3 MB (97.79%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -19,24 +19,17 @@ class Node {
 */
 
 class Solution {
-       public HashMap<Integer, Node> map = new HashMap<>();
+    public HashMap<Integer,Node> map = new HashMap<>();
     public Node cloneGraph(Node node) {
-        //going to create a hashmap to store node,val and nodes
-        //check if the node is already present in map 
-        //if it is there return the node
-        //else create a copy of that node
-        //then add ithe copynode to map
-        //run this for every neighbours of that node
-   
         if(node == null)return null;
-        if (map.containsKey(node.val)){
+        if(map.containsKey(node.val)){
             return map.get(node.val);
         }
-        Node copyNode = new Node(node.val,new ArrayList<Node>());
-        map.put(node.val,copyNode);
-        for(Node nei : node.neighbors){
-            copyNode.neighbors.add(cloneGraph(nei));
+        Node newNode = new Node(node.val,new ArrayList<Node>());
+        map.put(node.val,newNode);
+        for(Node nei: node.neighbors){
+            newNode.neighbors.add(cloneGraph(nei));
         }
-        return copyNode;
+        return newNode;
     }
 }
