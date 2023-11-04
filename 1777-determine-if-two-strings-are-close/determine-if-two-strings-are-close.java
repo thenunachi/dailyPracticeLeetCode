fbe550d99fb9b63cee9fb75b9c73732e59712ee5
Thenu <64111530+thenunachi@users.noreply.github.com>
2023-11-04 08:39:00 -0700

    Time: 12 ms (74.84%) | Memory: 44.4 MB (62.09%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -1,22 +1,20 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        if (word1.length() != word2.length())
-            return false;
-        int[] counts1 = new int[26];
-        int[] counts2 = new int[26];
-        int length = word1.length();
-        for (int i = 0; i < length; i++) {
-            char c1 = word1.charAt(i), c2 = word2.charAt(i);
-            counts1[c1 - 'a']++;
-            counts2[c2 - 'a']++;
-        }
-      for (int i = 0; i < 26; i++) {
-    if ((counts1[i] == 0 && counts2[i] != 0) || (counts1[i] != 0 && counts2[i] == 0))
-        return false;
-}
+        if(word1.length() != word2.length())return false;
+        int [] count1 = new int [26];
+          int [] count2= new int [26];
+          for(int i=0;i<word1.length();i++){
+              count1[word1.charAt(i) - 'a']++;
+              count2[word2.charAt(i) - 'a']++;
+          }
+          for(int i=0;i<count1.length;i++){
+              if(count1[i] != 0 && count2[i] ==0 || count1[i] == 0 && count2[i] !=0){
+                  return false;
+              }
+          }
+          Arrays.sort(count1);
+          Arrays.sort(count2);
+          return Arrays.equals(count1,count2);
 
-        Arrays.sort(counts1);
-        Arrays.sort(counts2);
-        return Arrays.equals(counts1, counts2);
     }
 }
