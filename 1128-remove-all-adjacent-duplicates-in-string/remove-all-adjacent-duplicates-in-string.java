d018fa3d346a6d2160140773f611efd0704602cf
Thenu <64111530+thenunachi@users.noreply.github.com>
2023-11-07 14:01:34 -0800

    Time: 101 ms (26.47%) | Memory: 44.6 MB (32.61%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -1,42 +1,19 @@
-// class Solution {
-//     public String removeDuplicates(String s) {
-//         Stack<Character>stack = new Stack<>();
-//         for(int i=0;i<s.length();i++){
-//             while(!stack.isEmpty()){
-//             if(stack.peek()!= s.charAt(i)){
-//                 stack.push(s.charAt(i));
-//             }
-//             else{
-//                 stack.pop();
-//                 stack.pop();
-//             }
-//         }
-//         stack.push(s.charAt(i));
-//         }
-//         String res = "";
-//         for(int i=0;i<stack.size();i++){
-// res+=stack[i];
-//         }
-//         return res;
-//     }
-// }
-
-import java.util.Stack;
-
 class Solution {
     public String removeDuplicates(String s) {
-        Stack<Character> stack = new Stack<>();
-        
-        for (int i = 0; i < s.length(); i++) {
-            if (!stack.isEmpty() && stack.peek() == s.charAt(i)) {
-                stack.pop();
-            } else {
-                stack.push(s.charAt(i));
+        Stack<Character>stack = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            
+            if(!stack.isEmpty() && stack.peek()== s.charAt(i)){
+               stack.pop();
             }
+            else{
+                
+             stack.push(s.charAt(i));
+            }
+
+   
         }
-        
-        // Create a StringBuilder to efficiently build the result string
-        StringBuilder result = new StringBuilder();
+       StringBuilder result = new StringBuilder();
         
         while (!stack.isEmpty()) {
             result.insert(0, stack.pop()); // Insert at the beginning to maintain original order
@@ -45,3 +22,28 @@ class Solution {
         return result.toString();
     }
 }
+
+// import java.util.Stack;
+
+// class Solution {
+//     public String removeDuplicates(String s) {
+//         Stack<Character> stack = new Stack<>();
+        
+//         for (int i = 0; i < s.length(); i++) {
+//             if (!stack.isEmpty() && stack.peek() == s.charAt(i)) {
+//                 stack.pop();
+//             } else {
+//                 stack.push(s.charAt(i));
+//             }
+//         }
+        
+//         // Create a StringBuilder to efficiently build the result string
+//         StringBuilder result = new StringBuilder();
+        
+//         while (!stack.isEmpty()) {
+//             result.insert(0, stack.pop()); // Insert at the beginning to maintain original order
+//         }
+        
+//         return result.toString();
+//     }
+// }
