2593e55e3ddab9a7ba548f66f64cd4616244cf19
Thenu <64111530+thenunachi@users.noreply.github.com>
2023-11-09 14:21:33 -0800

    Time: 0 ms (100.00%) | Memory: 48.4 MB (78.96%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.java
+++ 975-range-sum-of-bst/range-sum-of-bst.java
@@ -13,42 +13,30 @@
  *     }
  * }
  */
-// class Solution {
-//     public int rangeSumBST(TreeNode root, int low, int high) {
-//         int sum = 0;
-//         while(root != null){
-//             if(root.val <= low && root.val >= high){
-//                 sum+=root.val;
-//             }
-//             rangeSumBST( root.left,  low,  high);
-//             rangeSumBST( root.right,  low,  high);
-//         }
-//         return sum;
-//     }
-// }
-
 class Solution {
+    int sum = 0;
+
     public int rangeSumBST(TreeNode root, int low, int high) {
-        if (root == null) {
-            return 0;
+        if(root == null) return sum;
+
+        dfs(root, low, high);
+
+        return sum;
+    }
+
+    private void dfs(TreeNode root, int low, int high){
+        if(root == null) return;
+
+        if(root.val > low) {
+            dfs(root.left, low, high);
         }
 
-        int sum = 0;
-
-        if (root.val >= low && root.val <= high) {
+        if(root.val >= low && root.val <= high){
             sum += root.val;
         }
 
-        // If the current node's value is greater than the low bound, search the left subtree
-        if (root.val > low) {
-            sum += rangeSumBST(root.left, low, high);
+        if(root.val < high) {
+            dfs(root.right, low, high);
         }
-
-        // If the current node's value is less than the high bound, search the right subtree
-        if (root.val < high) {
-            sum += rangeSumBST(root.right, low, high);
-        }
-
-        return sum;
     }
 }
