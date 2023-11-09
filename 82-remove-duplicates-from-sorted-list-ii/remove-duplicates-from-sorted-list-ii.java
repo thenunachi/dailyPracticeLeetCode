7b178e24dede500e22668721ca014c1f52cff00f
Thenu <64111530+thenunachi@users.noreply.github.com>
2023-11-09 10:05:17 -0800

    Time: 0 ms (100.00%) | Memory: 42.8 MB (67.98%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
@@ -10,36 +10,24 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        // Create a dummy node and link it to the head of the input list.
-        ListNode dummy = new ListNode(0, head);
+         ListNode dummy = new ListNode(0, head);
         
         // Create a pointer 'prev' initialized to the dummy node.
         ListNode prev = dummy;
         
         // Create a pointer 'curr' initialized to the head of the input list.
         ListNode curr = head;
-        
-        // Iterate through the list until there are no more nodes to check.
-        while (curr != null && curr.next != null) {
-            // Check if the current node has a duplicate next node.
-            if (curr.val == curr.next.val) {
-                // Move 'curr' to the next non-duplicate node.
-                while (curr.next != null && curr.val == curr.next.val) {
-                    curr = curr.next;
-                }
-                
-                // Update 'prev' to skip the duplicate nodes.
-                prev.next = curr.next;
-            } else {
-                // Move 'prev' and 'curr' to the next nodes.
+        while(curr != null && curr.next != null){
+            if(curr.val == curr.next.val){
+                while(curr.next != null && curr.val == curr.next.val){
+                    curr =curr.next;
+                }prev.next =  curr.next;
+            }
+            else{
                 prev = prev.next;
             }
-            
-            // Move 'curr' to the next node.
             curr = curr.next;
         }
-        
-        // Return the next node of the dummy node, which represents the new head of the modified list.
         return dummy.next;
     }
 }
