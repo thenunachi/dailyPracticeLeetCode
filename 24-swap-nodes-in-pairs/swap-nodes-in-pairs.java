33c279d5d0a185eec8ccba4b04f2e9543c623a71
Thenu <64111530+thenunachi@users.noreply.github.com>
2023-11-09 09:37:04 -0800

    Time: 0 ms (100.00%) | Memory: 39.7 MB (91.20%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -10,32 +10,35 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        // Create a dummy node and link it to the head of the input list.
-        ListNode dummy = new ListNode(0, head);
-        
-        // Create a pointer 'prev' initialized to the dummy node.
+        ListNode dummy = new ListNode(0,head);
         ListNode prev = dummy;
-        
-        // Create a pointer 'curr' initialized to the head of the input list.
         ListNode curr = head;
-        
-        // Iterate through the list as long as there are at least two nodes to swap.
-        while (curr != null && curr.next != null) {
-            // Save pointers to the next pair and the second node in the pair.
-            ListNode nextPair = curr.next.next;
-            ListNode second = curr.next;
+        while(curr != null && curr.next != null){
+            ListNode fast = curr.next.next;
+            ListNode slow = curr.next;
 
-            // Reverse the pair by updating the next pointers.
-            second.next = curr;
-            curr.next = nextPair;
-            prev.next = second;
-
-            // Update the 'prev' and 'curr' pointers for the next iteration.
+            slow.next = curr;
+            curr.next = fast;
+            prev.next = slow;
+          
             prev = curr;
-            curr = nextPair;
+            curr = fast;
         }
-        
-        // Return the next node of the dummy node, which represents the new head of the modified list.
         return dummy.next;
     }
 }
+// Certainly! Let's go through the iterations of the `while` loop for the input linked list `[1,2,3,4]`:
+
+// | Iteration | curr  | curr.next      | prev  | nextPair | second | second.next |
+// |-----------|-------|----------------|-------|----------|--------|-------------|
+// | Initial   | 1     | 2              | dummy |          |        |             |
+// | 1         | 1     | 2              | dummy | 3        | 2      | 1           |
+// | 2         | 3     | 4              | 2     | null     | 4      | 3           |
+
+// In the initial state, `curr` points to the first node (1), and `curr.next` points to the second node (2). The `prev` pointer is initially set to the dummy node, and `nextPair` and `second` are yet to be defined.
+
+// After the first iteration, `curr` is still 1, `curr.next` is 2, `prev` is the dummy node, `nextPair` points to 3, `second` is 2, and `second.next` is 1.
+
+// After the second iteration, `curr` is now 3, `curr.next` is 4, `prev` is 2, `nextPair` is null (as there are no more pairs), `second` is 4, and `second.next` is 3.
+
+// This process continues until there are no more pairs to swap.
