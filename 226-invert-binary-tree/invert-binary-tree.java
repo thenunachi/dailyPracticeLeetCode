213f57e2f1dd27d7de9e77bdc4c419374df1d4c1
Thenu <64111530+thenunachi@users.noreply.github.com>
2023-11-16 13:12:45 -0800

    Time: 0 ms (100.00%) | Memory: 41.1 MB (5.05%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -15,14 +15,11 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if(root == null)return root;
-        TreeNode left = invertTree(root.left);
-        TreeNode right = invertTree(root.right);
-        //assign left = right and right = left
-        //how to do it?
-
-        root.left = right;
-        root.right = left;
-        return root;
+         if(root == null)return root;
+         TreeNode left = invertTree(root.left);
+         TreeNode right = invertTree(root.right);
+         root.left = right;
+         root.right = left;
+         return root;
     }
 }
