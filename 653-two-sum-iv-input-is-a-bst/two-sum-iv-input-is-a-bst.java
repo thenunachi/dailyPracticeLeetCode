244c09567f147b7bb7ae45cc202cd9e2209f17af
Thenu <64111530+thenunachi@users.noreply.github.com>
2023-11-16 09:54:24 -0800

    Time: 4 ms (23.39%) | Memory: 45.1 MB (6.46%) - LeetSync

---
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    Set<Integer> set = new HashSet<>();
+    
+    public boolean findTarget(TreeNode root, int k) {
+        
+        if (root == null) return false;
+        
+        if (set.contains(k - root.val)) return true;
+        else {
+            set.add(root.val);
+        }
+        
+        boolean left = findTarget(root.left, k);
+        boolean right = findTarget(root.right, k);
+        
+        if (left == true || right == true) return true;
+        return false;
+    }
+}
