44fdce73524b3ec84f8befe79e54594bbbcec918
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-05-30 14:05:42 -0700

    Time: 46 ms (53.45%) | Memory: 16.4 MB (83.43%) - LeetSync

--- 441-arranging-coins/arranging-coins.py
+++ 441-arranging-coins/arranging-coins.py
@@ -1,30 +1,14 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        # Initialize the left and right boundaries for binary search
-        l, r = 1, n
-        
-        # Initialize the result variable to store the maximum number of complete rows
-        res = 0
-        
-        # Perform binary search
-        while l <= r:
-            # Calculate the midpoint of the current search range
-            mid = (l + r) // 2
-            
-            # Calculate the number of coins required to form 'mid' complete rows
-            # Using the formula for the sum of the first 'mid' natural numbers: mid * (mid + 1) / 2
-            coins = (mid / 2) * (mid + 1)
-            
-            # If the calculated coins exceed the given number of coins 'n'
+        l = 0;
+        r = n;
+        res = 0;
+        while l <=r :
+            m = (l+r)//2;
+            coins = (m * (m+1))//2;
             if coins > n:
-                # Adjust the right boundary to search in the lower half
-                r = mid - 1
+                r = m -1 ;
             else:
-                # If the calculated coins are within the limit
-                # Adjust the left boundary to search in the upper half
-                l = mid + 1
-                # Update the result to the maximum number of complete rows found so far
-                res = max(mid, res)
-        
-        # Return the maximum number of complete rows that can be formed
-        return res
+                l = m+1;
+                res = max(res,m);
+        return res;
