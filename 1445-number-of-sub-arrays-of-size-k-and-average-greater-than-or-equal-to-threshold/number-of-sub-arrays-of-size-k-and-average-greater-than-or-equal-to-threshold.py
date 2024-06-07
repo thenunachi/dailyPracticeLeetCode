2d6aa63aac9761b7ce9e820eed2d5efae59463d5
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-06-07 10:57:06 -0700

    Time: 535 ms (6.38%) | Memory: 30 MB (14.30%) - LeetSync

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -1,26 +1,16 @@
 class Solution:
     def numOfSubarrays(self, nums: List[int], k: int, threshold: int) -> int:
-        if len(nums)<k:
-            return 0;
+        count =0
+        res = sum(nums[:k])
+        l = 0
+        if (res / k) >= threshold:
+            count+=1 
+        for r in range(k,len(nums)):
+            res +=nums[r] - nums[l]
+            l+=1
+            if (res / k) >= threshold:
+                print(r)    
+                count+=1
+            
+        return count
         
-        res = 0;
-        total = sum(nums[:k-1]);
-        
-        for r in range(len(nums)-k+1):            
-            total +=nums[r+k-1];
-            if total/k >= threshold:
-                res += 1                   
-            total -= nums[r]        
-           
-                
-        return res;
-# def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
-#         res = 0
-#         curSum = sum(arr[:k-1])
-
-#         for L in range(len(arr) - k + 1):
-#             curSum += arr[L + k - 1]
-#             if (curSum / k) >= threshold:
-#                 res += 1
-#             curSum -= arr[L]
-#         return res
