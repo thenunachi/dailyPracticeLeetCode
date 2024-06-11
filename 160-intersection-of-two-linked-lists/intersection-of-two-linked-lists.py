02b857ad8669e660e7026f2d9c71275ceadb8f37
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-06-11 10:30:34 -0700

    Time: 87 ms (23.63%) | Memory: 27.4 MB (20.70%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -6,28 +6,16 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        # l1,l2 = headA,headB
-        # while l1 != l2:
-        #     l1 = l1.next if l1 else headB
-        #     l2 = l2.next if l2 else headA
-         
-        # return l1
-        # # time O(n+m)
-   
         hashSet = set()
-        
-        # Traverse the first list and store its nodes in the set
-        curr = headA
-        while curr:
-            hashSet.add(curr)
-            curr = curr.next
-        
-        # Traverse the second list and check if any node is in the set
+        curr1 = headA
+        while curr1:
+            if curr1 not in hashSet:
+                hashSet.add(curr1)
+            curr1 = curr1.next
         curr2 = headB
         while curr2:
             if curr2 in hashSet:
                 return curr2
             curr2 = curr2.next
-        
-        # If no intersection is found, return None
         return None
+        
