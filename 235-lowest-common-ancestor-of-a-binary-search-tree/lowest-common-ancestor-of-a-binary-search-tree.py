0a1ea582186697647ff129e45cbaf5089889ba24
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-06-11 20:44:36 -0700

    Time: 52 ms (72.81%) | Memory: 20.3 MB (75.83%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,22 +7,14 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # Base case: if root is None, return None
         if not root:
             return None
-        
-        # If the current node is either p or q, return the current node
         if root == p or root == q:
             return root
-        
-        # Recur for the left and right subtree
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
-        
-        # If both left and right are non-null, it means p and q are found in
-        # different subtrees, so the current node is their LCA
+        left = self.lowestCommonAncestor(root.left,p,q) 
+        right = self.lowestCommonAncestor(root.right,p,q) 
+
         if left and right:
             return root
-        
-        # Otherwise, return the non-null value between left and right
         return left if left else right
+        
