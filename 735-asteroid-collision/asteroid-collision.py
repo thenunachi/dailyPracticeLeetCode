29a633ad3d8ef5f7fb2c61a90f1926bb60b164b5
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-06-25 20:10:00 -0700

    Time: 83 ms (51.90%) | Memory: 17.8 MB (79.28%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,15 +1,17 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         stack = []
+        
         for i in asteroids:
-            while stack and i<0<stack[-1]:
+            while stack and i < 0 < stack[-1]:
                 if stack[-1] < -i:
                     stack.pop()
-                    continue
+                    continue  # Continue checking the next element in the stack
                 elif stack[-1] == -i:
                     stack.pop()
-                break
+                    break  # Both are destroyed, exit the loop
+                break  # Current asteroid is destroyed, exit the loop
             else:
-                stack.append(i)
+                stack.append(i)  # Append the asteroid if no collision occurs
+                
         return stack
-        
