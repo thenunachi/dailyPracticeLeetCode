368417c557d8bad5196f07dd1231eaf951a34202
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-07-05 11:27:00 -0700

    Time: 45 ms (15.79%) | Memory: 16.5 MB (39.71%) - LeetSync

---
+++ 935-orderly-queue/orderly-queue.py
@@ -0,0 +1,13 @@
+class Solution:
+    def orderlyQueue(self, s: str, k: int) -> str:
+        if k == 1:
+            ans = s
+            for _ in range(len(s) - 1):
+                s = s[1:] + s[0]
+                ans = min(ans, s)
+            return ans
+        return "".join(sorted(s))
+#         The time complexity is 
+# , and the space complexity is 
+# . Here, 
+#  is the length of the string.
