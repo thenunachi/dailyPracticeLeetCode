e3f6b24bc2d7266e98e63a82969d0a9d53bc2dc4
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-07-05 09:16:31 -0700

    Time: 34 ms (73.30%) | Memory: 16.4 MB (55.67%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    
+    def rob(self, nums: List[int]) -> int:
+        # Edge cases
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+
+        # Compute the max by either excluding the first or the last house
+        return max(self.helper(nums[1:]), self.helper(nums[:-1]))
+
+    def helper(self, nums):
+        rob1, rob2 = 0, 0
+
+        for n in nums:
+            newRob = max(rob1 + n, rob2)
+            rob1 = rob2
+            rob2 = newRob
+        return rob2
+
+        
