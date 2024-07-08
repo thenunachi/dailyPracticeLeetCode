2746551920696759fd9e5a9ba745c5455341769f
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-07-08 13:31:16 -0700

    Time: 41 ms (89.39%) | Memory: 19.3 MB (20.24%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -9,13 +9,17 @@ class Solution:
         res = 0
         def dfs(root):
             nonlocal res
-            if not root :
+            if not root:
                 return 0
-            left = dfs( root.left)
-            right = dfs( root.right)
+            left =dfs(root.left)
+            right = dfs(root.right)
             res = max(res,left+right)
             return 1+max(left,right)
+        
+
 
         dfs(root)
         return res
-        
+
+
+   
