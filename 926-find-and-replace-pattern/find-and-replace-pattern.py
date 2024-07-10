5c401a317c0a77ade6a61960b9c11bf374411fd6
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-07-10 08:05:08 -0700

    Time: 37 ms (59.85%) | Memory: 16.7 MB (7.50%) - LeetSync

--- 926-find-and-replace-pattern/find-and-replace-pattern.py
+++ 926-find-and-replace-pattern/find-and-replace-pattern.py
@@ -1,16 +1,13 @@
 class Solution:
     def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
-        def match(w,p):
-            smap = [0]*128 
+        def match(s,t):
+            smap =[0]*128
             tmap = [0]*128
-           
-            for index,(charS,charT) in enumerate(zip(w,p),1):
+            for index,(charS,charT) in enumerate(zip(s,t),1):
                 if smap[ord(charS)] != tmap[ord(charT)]:
                     return False
-                smap[ord(charS)] = tmap[ord(charT)]= index
+                smap[ord(charS)] = tmap[ord(charT)] = index
             return True
-        
-
 
 
 
