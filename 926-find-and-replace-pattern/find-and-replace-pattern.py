d0ff3bf072f6ae7b8a9bd9cd39b34fbb704c66df
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-07-10 08:02:50 -0700

    Time: 44 ms (14.45%) | Memory: 16.4 MB (69.42%) - LeetSync

--- 926-find-and-replace-pattern/find-and-replace-pattern.py
+++ 926-find-and-replace-pattern/find-and-replace-pattern.py
@@ -1,22 +1,17 @@
 class Solution:
     def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
-        # Inner function to determine if the given string s matches the pattern t
-        def is_match(s, t):
-            # Initialize two maps for characters in s and t
-            # Each map stores the index of the character from s/t encountered in the string
-            map_s, map_t = [0] * 128, [0] * 128
-            # Enumerate through the pair(s) from s and t
-            for index, (char_s, char_t) in enumerate(zip(s, t), 1):
-                # If the mappings are not equal, then s does not match the pattern t
-                if map_s[ord(char_s)] != map_t[ord(char_t)]:
+        def match(w,p):
+            smap = [0]*128 
+            tmap = [0]*128
+           
+            for index,(charS,charT) in enumerate(zip(w,p),1):
+                if smap[ord(charS)] != tmap[ord(charT)]:
                     return False
-                # Update the mapping for the current character in both s and t to the current index
-                map_s[ord(char_s)] = map_t[ord(char_t)] = index
-            # If the loops completes without returning False, s matches the pattern t
+                smap[ord(charS)] = tmap[ord(charT)]= index
             return True
+        
 
-        # List comprehension to find and collect words that match the pattern
-        # It invokes the is_match function on each word with the given pattern
-        return [word for word in words if is_match(word, pattern)]
 
-        # O(N * K), where N is the length of the words list and K is the length of each word (and the pattern). 
+
+
+        return [word for word in words if match(word,pattern)]
