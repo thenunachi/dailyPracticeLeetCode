11c907ed2d31bcbb05c1b1353ced9930b8e51064
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-07-13 10:20:08 -0700

    Time: 45 ms (80.02%) | Memory: 19.3 MB (71.92%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.py
+++ 203-remove-linked-list-elements/remove-linked-list-elements.py
@@ -4,22 +4,13 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        # Create a dummy node that points to the head of the list
-        dummy = ListNode(0)
-        dummy.next = head
-        # Initialize current pointer to the dummy node
+        dummy = ListNode(0,head)
         curr = dummy
-        
-        # Traverse the list until the end
+
         while curr.next:
-            # If the next node's value matches the target value, skip it
             if curr.next.val == val:
                 curr.next = curr.next.next
             else:
-                # Otherwise, move the current pointer to the next node
                 curr = curr.next
-        
-        # Return the new head of the list, which is dummy.next
         return dummy.next
