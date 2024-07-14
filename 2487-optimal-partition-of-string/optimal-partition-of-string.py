a70a44e17871e060543cb0c4784f418f22913b8f
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-07-14 15:20:30 -0700

    Time: 82 ms (78.95%) | Memory: 17.3 MB (49.48%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.py
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.py
@@ -2,14 +2,13 @@ class Solution:
     def partitionString(self, s: str) -> int:
         res = 1
         hashset = set()
+
         for i in s:
             if i in hashset:
                 res +=1
-                hashset = set()
+                hashset =set()
                 hashset.add(i)
+            
             else:
                 hashset.add(i)
         return res
-
-        # time O(n)
-        # space O(1)
