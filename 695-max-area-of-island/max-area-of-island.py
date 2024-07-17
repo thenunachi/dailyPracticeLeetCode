c70b2ec151f30394251a45b232c38bad728f710c
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-07-17 10:14:50 -0700

    Time: 111 ms (51.93%) | Memory: 18.4 MB (17.26%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,59 +1,51 @@
-from typing import List
-class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        row = len(grid)
-        col = len(grid[0])
-        maxL = 0
-        visited = set()
-        count = 0
-        def dfs(i,j):
-            if i<0 or j <0 or i>= row or j >=col or grid[i][j] == 0:
-                return 0
-            if (i,j) in visited:
-                return 0
-            visited.add((i,j))
-            area = 1
-            area += dfs(i + 1, j)
-            area += dfs(i - 1, j)
-            area += dfs(i, j + 1)
-            area += dfs(i, j - 1)
-            return area
-
-
-
-        for i in range(row):
-            for j in range(col):
-                if grid[i][j] == 1 and (i,j) not in visited :
-                    count = dfs(i,j)
-                    maxL = max(maxL, count)
-        return maxL
-
-# from typing import List
-
 # class Solution:
 #     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-#         row = len(grid)
-#         col = len(grid[0])
-#         maxL = 0
-#         visited = set()
-        
-#         def dfs(i, j):
-#             if i < 0 or j < 0 or i >= row or j >= col or grid[i][j] == 0:
-#                 return 0
-#             if (i, j) in visited:
-#                 return 0
-#             visited.add((i, j))
-#             # Calculate area by visiting all connected cells
-#             area = 1
-#             area += dfs(i + 1, j)
-#             area += dfs(i - 1, j)
-#             area += dfs(i, j + 1)
-#             area += dfs(i, j - 1)
-#             return area
+        # rows = len(grid)
+        # cols = len(grid[0])
 
-#         for i in range(row):
-#             for j in range(col):
-#                 if grid[i][j] == 1 and (i, j) not in visited:
-#                     count = dfs(i, j)
-#                     maxL = max(maxL, count)
-#         return maxL
+        # visit = set()
+        # maxL = 0
+        # def dfs(i,j):
+        #     if i < 0 or j<0 or i >= rows or j>= cols or (i,j) in visit or grid[i][j] == 0:
+        #         return
+        #     visit.add((i,j))
+        #     area =1
+        #     area += dfs(i,j+1)
+        #     area += dfs(i,j-1)
+        #     area += dfs(i+1,j)
+        #     area += dfs(i-1,j)
+
+        #     return area
+
+
+        # for i in range(rows):
+        #     for j in range(cols):
+        #         if grid[i][j] == 1 and (i,j) not in visit:
+        #             maxL = max(maxL ,dfs(i,j))
+        # return maxL
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+
+        visit = set()
+        maxL = 0
+
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >= rows or j >= cols or (i, j) in visit or grid[i][j] == 0:
+                return 0  # Return 0 in the base case
+            visit.add((i, j))
+            area = 1  # Initial area for the current cell
+            area += dfs(i, j + 1)
+            area += dfs(i, j - 1)
+            area += dfs(i + 1, j)
+            area += dfs(i - 1, j)
+
+            return area  # Return the accumulated area
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1 and (i, j) not in visit:
+                    maxL = max(maxL, dfs(i, j))
+        
+        return maxL
