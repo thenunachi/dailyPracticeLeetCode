339528f228185ba7f8b5ba658f68d7b260931fce
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-07-18 10:12:05 -0700

    Time: 95 ms (26.94%) | Memory: 22 MB (19.99%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -6,14 +6,13 @@ class Solution:
 
         for i in nums:
             hashmap[i] = 1+ hashmap.get(i,0)
+        
 
-        for key,values in hashmap.items():
-            freq[values].append(key)
+        for keys,values in hashmap.items():
+            freq[values].append(keys)
 
         for i in range(len(freq)-1,0,-1):
-            # print(freq) [[], [3], [2], [1], [], [], []]
             for n in freq[i]:
-                # print(n)
                 res.append(n)
                 if len(res) == k:
                     return res
