f33ac7c91d3009b51e11937b4434cba92d793bb7
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-07-23 13:29:56 -0700

    Time: 256 ms (43.70%) | Memory: 28 MB (5.44%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,22 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = set()
-        row = len(grid)
-        col = len(grid[0])
-        count = 0
-        def dfs(i,j):
-            if i<0 or j < 0 or i>= row or j >= col or grid[i][j] == '0':
+        rows = len(grid)
+        cols = len(grid[0])
+        v = set()
+        count =0
+
+
+        def dfs(r,c):
+            if r<0 or r >= rows or c <0 or c>=cols or (r,c) in v or grid[r][c] == "0":
                 return
-            if (i,j) in visited:
-                return
-            visited.add((i,j))
-            dfs(i+1,j)
-            dfs(i-1,j)
-            dfs(i,j+1)
-            dfs(i,j-1)
-        for i in range(row):
-            for j in range(col):
-                if grid[i][j] == '1' and (i,j) not in visited:
+            v.add((r,c))
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+
+
+
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == "1" and (i,j) not in v:
+                    count+=1
                     dfs(i,j)
-                    count +=1
         return count
