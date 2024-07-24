5b343b61bdc4ccc127a143d44950cd1e91578114
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-07-24 09:21:56 -0700

    Time: 118 ms (32.91%) | Memory: 18.3 MB (17.80%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,51 +1,27 @@
-# class Solution:
-#     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        # rows = len(grid)
-        # cols = len(grid[0])
-
-        # visit = set()
-        # maxL = 0
-        # def dfs(i,j):
-        #     if i < 0 or j<0 or i >= rows or j>= cols or (i,j) in visit or grid[i][j] == 0:
-        #         return
-        #     visit.add((i,j))
-        #     area =1
-        #     area += dfs(i,j+1)
-        #     area += dfs(i,j-1)
-        #     area += dfs(i+1,j)
-        #     area += dfs(i-1,j)
-
-        #     return area
-
-
-        # for i in range(rows):
-        #     for j in range(cols):
-        #         if grid[i][j] == 1 and (i,j) not in visit:
-        #             maxL = max(maxL ,dfs(i,j))
-        # return maxL
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        count = 0
+        maxL = 0
         rows = len(grid)
         cols = len(grid[0])
+        v = set()
 
-        visit = set()
-        maxL = 0
+        def dfs(i,j):
+            if i<0 or i>= rows or j <0 or j>= cols or grid[i][j] == 0 or (i,j) in v:
+                return 0
+            v.add((i,j))
+            area =1
+            area += dfs(i+1,j)
+            area += dfs(i-1,j)
+            area += dfs(i,j+1)
+            area += dfs(i,j-1)
 
-        def dfs(i, j):
-            if i < 0 or j < 0 or i >= rows or j >= cols or (i, j) in visit or grid[i][j] == 0:
-                return 0  # Return 0 in the base case
-            visit.add((i, j))
-            area = 1  # Initial area for the current cell
-            area += dfs(i, j + 1)
-            area += dfs(i, j - 1)
-            area += dfs(i + 1, j)
-            area += dfs(i - 1, j)
+            return area
 
-            return area  # Return the accumulated area
 
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j] == 1 and (i, j) not in visit:
-                    maxL = max(maxL, dfs(i, j))
-        
+                if grid[i][j] == 1 and (i,j) not in v:
+                    count = dfs(i,j)
+                    maxL = max(maxL,count)
         return maxL
