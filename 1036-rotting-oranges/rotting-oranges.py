9dbdcb4bfa17c09071b8803cb46662e165ec0a35
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-07-24 18:04:36 -0700

    Time: 42 ms (93.67%) | Memory: 16.6 MB (15.94%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -2,36 +2,40 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         rows = len(grid)
         cols = len(grid[0])
-        freshOrange = 0
+        freshOranges = 0
         minPassed = 0
-        visit =set()
+        v = set()
         q = deque()
-        def bfs(i,j):
-            nonlocal freshOrange
-            if i < 0 or j<0 or i>= rows or j>= cols or (i,j)in visit or grid[i][j] == 0:
+        def bfs(r,c):
+            nonlocal freshOranges
+            if r <0 or c < 0 or r>=rows or c>=cols or (r,c) in v or grid[r][c] == 0:
                 return
-            visit.add((i,j))
-            q.append((i,j))
-            if grid[i][j] ==1:
-                freshOrange -=1
-                grid[i][j] = 2
-
-
+            v.add((r,c))
+            q.append((r,c))
+            if grid[r][c] == 1:
+                freshOranges -=1
+                grid[r][c] = 2
+            
+            
 
 
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == 2:
-                    visit.add((i,j))
+                    v.add((i,j))
                     q.append((i,j))
-                if grid[i][j] ==1:
-                    freshOrange +=1
-        while q and freshOrange >0:
+                if grid[i][j]==1:
+                    freshOranges +=1
+
+        while q and freshOranges >0:
             minPassed +=1
             for i in range(len(q)):
                 r,c = q.popleft()
+                
                 bfs(r+1,c)
                 bfs(r-1,c)
                 bfs(r,c+1)
                 bfs(r,c-1)
-        return minPassed if freshOrange ==0 else  -1
+           
+
+        return minPassed if freshOranges == 0 else -1
