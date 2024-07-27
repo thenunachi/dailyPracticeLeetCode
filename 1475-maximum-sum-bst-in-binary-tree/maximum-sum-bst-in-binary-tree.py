7f03fe8ad3ed323499c03fb8159cd667a73ae0e0
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-07-27 09:45:46 -0700

    Time: 256 ms (61.64%) | Memory: 35.3 MB (81.58%) - LeetSync

---
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxSumBST(self, root: Optional[TreeNode]) -> int:
+        res = 0
+        def recur(node):
+            nonlocal res
+            if not node: return True, inf, -inf, 0
+            s1, mn1, mx1, sm1 = recur(node.left)
+            s2, mn2, mx2, sm2 = recur(node.right)
+            if s1 and s2 and mx1 < node.val < mn2: res = max(res, sm1+sm2+node.val)
+            return s1 and s2 and mx1 < node.val < mn2, min(mn1, mn2, node.val), max(mx1, mx2, node.val), sm1+sm2+node.val
+        recur(root)
+        return res
