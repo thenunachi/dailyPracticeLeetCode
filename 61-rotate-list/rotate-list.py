f4fc3523ca1e9ed331521efc44466304cd3cd127
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-07-30 10:41:39 -0700

    Time: 26 ms (98.84%) | Memory: 16.6 MB (58.54%) - LeetSync

--- 61-rotate-list/rotate-list.py
+++ 61-rotate-list/rotate-list.py
@@ -5,28 +5,21 @@
 #         self.next = next
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        if not head : return head
-        length = 1
+        if not head:
+            return head
         tail = head
+        length =1
         while tail.next:
             tail = tail.next
-            length+=1
-          # Compute the effective number of rotations needed, 
-        # as rotating a list by its length results in the same list
-        k = k % length
-        
-        # If no rotation is needed, return the head as it is
-        if k == 0:
+            length +=1
+        k = k%length
+        if k ==0:
             return head
         curr = head
         for i in range(length -k -1):
             curr = curr.next
-        tmp = curr.next
+        tmp =curr.next
         curr.next = None
         tail.next = head
 
         return tmp
-
-
-
-        
