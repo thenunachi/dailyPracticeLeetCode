245c42f45b75a84e10af0eddf32344b89f55e0c0
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-07-30 11:26:19 -0700

    Time: 40 ms (80.04%) | Memory: 16.8 MB (35.63%) - LeetSync

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -2,14 +2,11 @@ class Solution:
     def calPoints(self, operations: List[str]) -> int:
         stack = []
         for i in operations:
-            if i == "+"and len(stack) >=2:
-                t1 = stack[-1]
-                t2 = stack[-2]
-                stack.append((t1+t2))
-            elif i == "D"and len(stack) >=1:
-                t1 = stack[-1]
-                stack.append((t1*2))
-            elif i == "C"and len(stack) >=1:
+            if i == "+" and len(stack )>=2:
+                stack.append((stack[-1] +stack[-2]))
+            elif i == "D" and len(stack) >=1:
+                stack.append(2*stack[-1])
+            elif i == "C" and len(stack)>=1:
                 stack.pop()
             else:
                 stack.append(int(i))
