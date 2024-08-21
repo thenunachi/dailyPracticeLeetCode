7aafc81dae986b1c0e2666992ac8713fa3f5d2f7
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-08-21 13:56:03 -0700

    Time: 33 ms (50.24%) | Memory: 11.9 MB (89.48%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -1,23 +1,50 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> bool:
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        # clarification O/p boolean
+        # test cases
+        # Implement
+        # verify
+
+        # normal binary search
+        # l = 0 
+        # r= len(nums)-1
+        # while l<=r:
+        #     m = (l+r)//2
+        #     if nums[m] == target:
+        #         return true
+        #     if nums[m] < target:
+        #         # move the left
+        #         l = m+1
+        #     else:
+        #          r = m-1
+        # return false
         l = 0
         r = len(nums)-1
-        while l<=r:
+        while l <= r:
             m = (l+r)//2
             if nums[m] == target:
                 return True
-             # Handling the case where we cannot be sure about the sorted part
             if nums[l] == nums[m] == nums[r]:
                 l += 1
                 r -= 1
-            elif nums[l] <= nums[m]:  # Left part is sorted
-                if nums[l] <= target < nums[m]:
-                    r = m - 1
+            elif nums[l] <= nums[m]:
+                if nums[l] <=target < nums[m]:
+                    r = m-1
                 else:
-                    l = m + 1
-            else:  # Right part is sorted
+                    l = m+1
+
+
+            else:
                 if nums[m] < target <= nums[r]:
-                    l = m + 1
+                    l = m+1
                 else:
-                    r = m - 1
+                    r = m-1
+
         return False
+
+        
