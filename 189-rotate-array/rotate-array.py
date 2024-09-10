d9ad45c633574cb636ec5d126f1ceacea2b9b7e7
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-09-10 09:49:53 -0700

    Time: 164 ms (15.50%) | Memory: 23.1 MB (86.40%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,25 +1,33 @@
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
+class Solution(object):
+    def rotate(self, nums, k):
         """
-        Do not return anything, modify nums in-place instead.
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
         """
-        k = k % len(nums)
-        l=0
-        r =len(nums)-1
+        k = k%len(nums)
+        l = 0
+        r = len(nums)-1
         while l<r:
-            nums[l] , nums[r] = nums[r] , nums[l]
+            tmp = nums[r]
+            nums[r] = nums[l]
+            nums[l] = tmp
             l+=1
             r-=1
-        
-        l=0
-        r =k -1
+        l = 0
+        r = k-1
         while l<r:
-            nums[l] , nums[r] = nums[r] , nums[l]
+            tmp = nums[r]
+            nums[r] = nums[l]
+            nums[l] = tmp
             l+=1
             r-=1
-        l=k
-        r =len(nums)-1
+        l = k
+        r = len(nums)-1
         while l<r:
-            nums[l] , nums[r] = nums[r] , nums[l]
+            tmp = nums[r]
+            nums[r] = nums[l]
+            nums[l] = tmp
             l+=1
             r-=1
+            
