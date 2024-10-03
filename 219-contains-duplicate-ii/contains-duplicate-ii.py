24e3252399647be2f18dad7828c8372e486081f4
Thenu <64111530+thenunachi@users.noreply.github.com>
2024-10-03 10:55:12 -0700

    Time: 467 ms (82.19%) | Memory: 22.1 MB (79.97%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -5,17 +5,27 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
-        s = set()
-        l = 0
-        for r in range(len(nums)):
-            if r-l > k:
-                s.remove(nums[l])
-                l+=1
-            if nums[r] in s:
-                return True
-            else:
-                s.add(nums[r])
+        # s = set()
+        # l = 0
+        # for r in range(len(nums)):
+        #     if r-l > k:
+        #         s.remove(nums[l])
+        #         l+=1
+        #     if nums[r] in s:
+        #         return True
+        #     else:
+        #         s.add(nums[r])
+        # return False
+
+        map = {}
+
+        for i,n in enumerate(nums):
+            if n in map:
+                if (i-map[n]) <= k:
+                    return True
+            map[n] = i
         return False
+
             
 
 
