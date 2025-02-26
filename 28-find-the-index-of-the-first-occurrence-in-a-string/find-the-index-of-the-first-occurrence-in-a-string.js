30ccba5bd352c21924913da553b2e21ad4f68751
Thenu <64111530+thenunachi@users.noreply.github.com>
2025-02-25 16:32:35 -0800

    Time: 0 ms (100.00%) | Memory: 53.9 MB (7.28%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+   
+    let index = haystack.indexOf(needle);
+    return index
+   
+};
