fef1620a2465b8a7ba210339ea2179641d29c3aa
Thenu <64111530+thenunachi@users.noreply.github.com>
2025-02-28 10:24:22 -0800

    Time: 11 ms (87.68%) | Memory: 24.5 MB (39.39%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -10,9 +10,22 @@ class Solution(object):
         #     if nums1[i] in nums2:
         #         arr.append(nums1[i])
         # return min(arr) if arr else -1
-        set_nums2 = set(nums2)  # Convert nums2 to a set for O(1) membership checking
-        common = []
-        for num in nums1:
-            if num in set_nums2:
-                common.append(num)
-        return min(common) if common else -1
+        # set_nums2 = set(nums2)  # Convert nums2 to a set for O(1) membership checking
+        # common = []
+        # for num in nums1:
+        #     if num in set_nums2:
+        #         common.append(num)
+        # return min(common) if common else -1
+        i, j = 0, 0
+        
+        # Use two pointers to traverse the arrays.
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                # Found a common element; it's the smallest one because arrays are sorted.
+                return nums1[i]
+                
+        return -1
