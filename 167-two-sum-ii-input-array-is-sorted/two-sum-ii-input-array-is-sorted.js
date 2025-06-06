a72d8e582c8ac58793feee9bc252db208991ee44
Thenu <64111530+thenunachi@users.noreply.github.com>
2025-06-06 10:28:00 -0700

    Time: 3 ms (37.46%) | Memory: 56 MB (56.79%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -3,19 +3,19 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(numbers, target) {
+var twoSum = function (numbers, target) {
     let l = 0;
-    let r = numbers.length-1;
-  
-    while (l < r){
-if (numbers[l]+ numbers[r] > target){
-    r--;
-}
-else if (numbers[l]+ numbers[r] < target){
-    l++
-}else{
-    return [l + 1, r + 1];
-}
+    let r = numbers.length - 1;
+
+    while (l < r) {
+        if (numbers[l] + numbers[r] > target) {
+            r--;
+        }
+        else if (numbers[l] + numbers[r] < target) {
+            l++
+        } else {
+            return [l + 1, r + 1];
+        }
     }
-   
+
 };
