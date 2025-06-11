df158e63ba77e8ab3a44ed0e87bb11beac77f1f1
Thenu <64111530+thenunachi@users.noreply.github.com>
2025-06-11 13:11:28 -0700

    Time: 62 ms (28.57%) | Memory: 113 MB (24.85%) - LeetSync

--- 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.js
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.js
@@ -10,25 +10,20 @@
  * @return {ListNode}
  */
 var mergeNodes = function(head) {
-  
-  let dummy = new ListNode(-1);
-  let curr = dummy;
-  let sum = 0;
-
-  head = head.next; // Skip the initial 0
-
-  while (head) {
-    if (head.val === 0) {
-      // When we reach a 0, we add the sum as a new node if sum > 0
-      curr.next = new ListNode(sum);
-      curr = curr.next;
-      sum = 0;
-    } else {
-      sum += head.val;
-    }
+    let dummy = new ListNode(-1);
+    let curr = dummy
+    let sum = 0;
     head = head.next;
-  }
-
-  return dummy.next;
-
+    while(head){
+        if(head.val == 0){
+            curr.next = new ListNode(sum);
+            curr = curr.next;
+            sum = 0
+        }
+        else{
+            sum += head.val
+        }
+        head = head.next;
+    }
+    return dummy.next;
 };
