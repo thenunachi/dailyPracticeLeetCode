69d4c06203967662715b764edeff7ba0d15defb3
Thenu <64111530+thenunachi@users.noreply.github.com>
2025-07-01 20:03:33 -0700

    Time: 25 ms (72.84%) | Memory: 69.4 MB (93.76%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    let set = new Set();
+
+    for (let i = 0; i < nums.length; i++) {
+        if (set.has(nums[i])) return true;
+
+        set.add(nums[i]);
+
+        // Maintain the window of size k
+        if (set.size > k) {
+            set.delete(nums[i - k]);
+        }
+    }
+
+    return false;
+};
