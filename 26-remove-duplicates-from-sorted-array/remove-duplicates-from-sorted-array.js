d0d3b4611fd12565455c8a5655e7ea1585acec7c
Thenu <64111530+thenunachi@users.noreply.github.com>
2025-07-11 11:41:10 -0700

    Time: 1 ms (58.70%) | Memory: 56.2 MB (91.66%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -4,14 +4,11 @@
  */
 var removeDuplicates = function(nums) {
     let l = 0
-    for(let r = 1; r< nums.length; r++){
+    for(let r= 1; r< nums.length; r++){
         if(nums[l] != nums[r]){
             l++
             nums[l] = nums[r]
-          
-            console.log(nums)
         }
-       
     }
     return l+1
 };
