e48d1b3914d77f74a0ce8e6b2d377f2bcc1cb5fa
Thenu <64111530+thenunachi@users.noreply.github.com>
2025-07-24 10:58:34 -0700

    Time: 34 ms (92.48%) | Memory: 53.7 MB (62.13%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.js
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -4,26 +4,22 @@
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-    //similar to merge linkedlist
-    let output = ""
+    let s = ""
     let l = 0
-    let r = 0
-    while (l < word1.length && r < word2.length){
-        
-          output += word1[l];
-          output += word2[r];
-          l++
-          r++
-        
-        
-    }
-    while(l < word1.length){
-         output += word1[l]; 
-         l++
-    }
-    while(r < word2.length){
-         output += word2[r];
-         r++ 
-    }
-    return output;
+     let r = 0
+     while(l < word1.length && r < word2.length){
+        s += word1[l]
+        s+= word2[r]
+        l++
+        r++
+     }
+     while(l < word1.length){
+        s+= word1[l]
+        l++
+     }
+     while(r< word2.length){
+        s+= word2[r]
+        r++
+     }
+     return s;
 };
