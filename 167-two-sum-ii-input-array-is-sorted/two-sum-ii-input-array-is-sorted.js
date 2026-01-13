38591471159647ebe344eb904b1c0a1cac23c52e
Thenu <64111530+thenunachi@users.noreply.github.com>
2026-01-12 18:19:05 -0800

    Time: 1 ms (82.21%) | Memory: 56.8 MB (20.68%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -3,19 +3,24 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (numbers, target) {
-    let l = 0;
-    let r = numbers.length - 1;
+var twoSum = function(numbers, target) {
+    //will have two pointers which one starts from beginning and another starts from end
+    //then will have a while loop add the two indices of the numbser and check equal to target
+    //if equal to target return the indices+1 in the array
+    //else return -1,-1
 
-    while (l < r) {
-        if (numbers[l] + numbers[r] > target) {
-            r--;
-        }
-        else if (numbers[l] + numbers[r] < target) {
-            l++
-        } else {
-            return [l + 1, r + 1];
-        }
+    let l = 0
+    let r = numbers.length-1
+   while(l < r ){
+    if(numbers[l] + numbers[r] > target){
+        r -=1
     }
-
+    else if (numbers[l] + numbers[r] < target){
+        l += 1
+    }
+    else{
+        return [l+1 , r+1]
+    }
+   }
+   return [-1,-1]
 };
