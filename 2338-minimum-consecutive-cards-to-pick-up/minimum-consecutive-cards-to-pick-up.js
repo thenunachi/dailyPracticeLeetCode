3dfad7debf34cec435b991c2f5d45db3e80e46da
Thenu <64111530+thenunachi@users.noreply.github.com>
2026-02-02 10:43:22 -0800

    Time: 597 ms (6.06%) | Memory: 82.6 MB (81.82%) - LeetSync

---
+++ 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} cards
+ * @return {number}
+ */
+var minimumCardPickup = function(cards) {
+    let l = 0
+    let minLength = Infinity
+    let s = new Set()
+    for(let r = 0; r < cards.length; r++){
+        while(s.has(cards[r])){
+           minLength = Math.min(minLength,r-l+1)
+           console.log(minLength,"m")
+           s.delete(cards[l])
+           l++
+        }
+        s.add(cards[r])
+    }
+    return minLength === Infinity ? -1 : minLength
+};
